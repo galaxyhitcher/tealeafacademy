c56b2c032af6af9de53787bd4f907bfc31c30617
galaxyhitcher <louis.heimel@mines.sdsmt.edu>
2015-03-04 17:52:30 -0700

    Finished exercises for Variables chapter

---
+++ IntroductiontoProgrammingwithRuby/Variables/age.rb
@@ -0,0 +1,11 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+age_array = [10,20,30,40]
+
+age_array.each do |years|
+
+  puts "In " + years.to_s + " years you will be:"
+  puts age + years
+
+end
+
---
+++ IntroductiontoProgrammingwithRuby/Variables/name.rb
@@ -0,0 +1,17 @@
+puts "Type your name why doncha?"
+name = gets.chomp
+puts "Hey there " + name + "!"
+
+10.times do 
+  puts name
+end
+
+puts "What is your first name?"
+
+first_name = gets.chomp
+
+puts "What is your last name?"
+
+last_name = gets.chomp
+
+puts first_name + " " + last_name
