e6295a7b4f70bbdf5816a3f888810afca51cdbbc
galaxyhitcher <louis.heimel@mines.sdsmt.edu>
2015-03-04 17:01:50 -0700

    Completed exercises at the end of The Basics

---
+++ IntroductiontoProgrammingwithRuby/TheBasics/six.rb
@@ -0,0 +1,3 @@
+puts '5.21 squared equals ' + (5.21*5.21).to_s
+puts '3.14159 squared equals ' + (3.14159*3.14159).to_s
+puts '2.71 squared equals ' + (2.71*2.71).to_s
---
+++ IntroductiontoProgrammingwithRuby/TheBasics/three.rb
@@ -0,0 +1,4 @@
+{"Driving Miss Daisy" => 1989,"Dances With Wolves"=>1990,"The Silence of the Lambs"=>1991,"Unforgiven"=>1992,"Schindler's List"=>1993,"Forrest Gump"=>1994,"Braveheart"=>1995}.each do |movie, year|
+  puts year
+end
+
