9f848bee09bff97942beca4e9c6a7353e91b018e
galaxyhitcher <louis.heimel@mines.sdsmt.edu>
2015-03-28 22:58:27 -0600

    finished calculator.rb

---
+++ Course1/lesson1/calculator.rb
@@ -0,0 +1,42 @@
+def speak str
+  puts "=> " + str
+  puts
+end
+
+
+#the while loop condition checks if a and b are integers
+a = String.new
+speak "Please enter integers!"
+
+speak "What is your first number?"
+
+a = gets.chomp
+
+speak "What is your second number?"
+
+b = gets.chomp
+
+a = a.to_i
+b = b.to_i
+
+speak "Enter the operation you would like to perform on a and b: +, -, /, *"
+
+op = gets.chomp
+
+if op == "+"
+  puts "#{a} + #{b} = #{a+b}"
+elsif op == "-"
+  puts "#{a} - #{b} = #{a-b}"
+elsif op == "/"
+  puts "#{a} / #{b} = #{a.to_f/b.to_f}"
+elsif op == "*"
+  puts "#{a} * #{b} = #{a*b}"
+end
+
+
+exit = "N"
+
+while exit != "Y"
+  puts "exit? (Y/N)"
+  exit = gets.chomp
+end
