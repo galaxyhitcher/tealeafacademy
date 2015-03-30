7bfdf69e554e85c8d0fb20c5b33c44faa7e7dfeb
galaxyhitcher <louis.heimel@mines.sdsmt.edu>
2015-03-29 19:33:05 -0600

    modified rockpaperscissors.rb

--- Course1Lesson1/rockpaperscissors.rb
+++ Course1Lesson1/rockpaperscissors.rb
@@ -1,67 +1,39 @@
-finished = "N"
+#1. All players pick either paper, rock, or scissors
+#2. compare paper>rock, rock>scissors, scissors> paper, tie if same
+#3.
+CHOICES = {'p'=>"Paper",'r'=>"Rock",'s'=>"Scissors"}
+puts "Play Paper, Rock, Scissors!"
 
-while finished == "N"
-  user_choice = String.new
-   while user_choice.upcase != "P" and user_choice.upcase != "R" and user_choice != "S"
-    puts "Play Paper Rock Scissors!"
-    puts "Choose one: (P/R/S)"
-
-    user_choice = gets.chomp.upcase
-
+def display_winning_message(winning_choice)
+  case winning_choice
+  when 'p'
+    puts "Paper wraps Rock!"
+  when 'r'
+    puts "Rock smashes Scissors!"
+  when 's'
+    puts "Scissors cuts Paper!"
   end
-
-  computer_choice = ["P","R","S"].sample
-
-
-  if user_choice == "P"
-    if computer_choice == "P"
-      puts "You picked Paper and computer picked Paper."
-      puts "It's a tie."
-    elsif computer_choice == "R"
-      puts "You picked Paper and computer picked Rock."
-      puts "Paper wraps Rock!"
-      puts "You won!"
-    elsif computer_choice == "S"
-      puts "You picked Paper and computer picked Scissors."
-      puts "Scissors cut Paper!"
-      puts "Computer won!"
-    end
-  elsif user_choice == "R"
-    if computer_choice == "P"
-      puts "You picked Rock and computer picked Paper."
-      puts "Paper wraps Rock!"
-      puts "Computer won!"
-    elsif computer_choice == "R"
-      puts "You picked Rock and computer picked Rock."
-      puts "It's a tie."
-    elsif computer_choice == "S"
-      puts "You picked Rock and computer picked Scissors."
-      puts "Rock smashes Scissors!"
-      puts "You won!"
-    end
-  elsif user_choice == "S"
-    if computer_choice == "P"
-      puts "You picked Scissors and computer picked Paper."
-      puts "Scissors cut Paper."
-      puts "You won!"
-    elsif computer_choice == "R"
-      puts "You picked Scissors and computer picked Rock."
-      puts "Rock smashes Scissors!"
-      puts "Computer won!"
-    elsif computer_choice == "S"
-      puts "You picked Scissors and computer picked Scissors."
-      puts "It's a tie."
-    end
-  end
-  puts "Are you finished, or do you want to play again? (Y/N)?"
-  finished = gets.chomp.upcase
 end
 
+loop do
+  #player makes a pick
+  begin
+    puts "Pick one: (p,r,s)"
+    player_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
 
-#Paper wraps Rock!
-#You won!
+  computer_choice = CHOICES.keys.sample
 
-#You picked Scissors and the computer picked Scissors
-#It's a tie.
+  if player_choice == computer_choice
+    puts "It's a tie!"
+  elsif (player_choice == 'p' && computer_choice == 'r')||(player_choice == 'r' && computer_choice == 's')||(player_choice == 's' && computer_choice == 'p')
+    display_winning_message(player_choice)
+    puts "You won!"
+  else
+    display_winning_message(computer_choice)
+    puts "Computer won!"
+  end
 
-#Play again? (Y/N)
+  puts "Play again? (y/n)"
+  break if gets.chomp.downcase != 'y'
+end
