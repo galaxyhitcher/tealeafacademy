9b7f5878cba6c6da482702ba77623c45ee2853fa
galaxyhitcher <gregoryheimel@PIPESTONEs.local>
2015-05-16 12:40:09 -0500

    made modifications to Object Oriented Tic Tac Toe game

--- Course1Lesson2/oo_ttt.rb
+++ Course1Lesson2/oo_ttt.rb
@@ -5,7 +5,7 @@ class Competitor
 	end
 
   def mark_board(board,pos)
-		board.mark(pos,@symbol)
+   	board.mark(pos,@symbol)
 	end
 end
 
@@ -41,96 +41,121 @@ class Board
 	def mark(pos,marker)
 		@state[pos] = marker
 	end
-	
-	def combo(a,b,c)
-	    a, b, c = a.to_i, b.to_i, c.to_i
-	    if @state[a] == @state[b] && @state[b] == @state[c] && @state[a] != ' '
-	        return true
-	    else
-	        return false
-	    end
+
+	def available_moves
+
+		available_moves = []
+
+		(0..8).each do |i|
+			if self.state[i] == ' '
+				available_moves << i
+			end
+		end
+
+		available_moves
+
 	end
-	
-	def win?
-	    if combo(0,1,2) || combo(3,4,5) || combo(6,7,8) || combo(0,3,6) || combo(1,4,7) || combo(2,5,8) || combo(0,4,8) || combo(2,4,6)
-	        return true
-	    else
-	        return false
-	    end
-	end
-	
-	def tied?
-	    if !win?
-	        #check to see if board is filled
-	        @state.each do |pos|
-	            if pos == ' '
-	                return false
-	            end
-	        end
-	        return true
-	    end
-	end
+
 end
 
 class Game
-	attr_accessor :completed
+	attr_accessor :completed, :gameboard, :turn
+
 	def initialize
 		@completed = false
 		@gameboard = Board.new
 	end
 
-	def play
-		human = Player.new
-		comp = Computer.new
-		winner = "Computer"
-		
-		#until the game is won or tied, player takes a turn, then computer takes a turn
-
-		#print the board
-		#ask the player where he wants to move
-		#if the space is occupied, tell the user to pick another space
-
-		#computer picks an unoccupied space
-
-		until @gameboard.win? || @gameboard.tied?
-			occupied = true
-			while occupied == true
-				@gameboard.print
-				puts "where do you want to move?"
-				pos = gets.chomp.to_i
-				if @gameboard.state[pos] == ' '
-					@gameboard.state[pos] = human.symbol
-					occupied = false
-				else
-					puts "this spot is occupied, pick another"
-				end
-			end
-
-			if @gameboard.win? || @gameboard.tied?
-				winner = "You"
-				break
-			end
-
-			
-
-			unoccupied = []
-			(0..8).each do |i|
-				if @gameboard.state[i] == ' '
-					unoccupied << i
-				end
-			end
-
-			@gameboard.state[unoccupied.sample] = comp.symbol
-
-		end
-
-		@gameboard.print
-		puts winner + " Won!"
+	def combo(a,b,c)
+	    a, b, c = a.to_i, b.to_i, c.to_i
+	    if @gameboard.state[a] == @gameboard.state[b] && @gameboard.state[b] == @gameboard.state[c] && @gameboard.state[a] != ' '
+	      true
+	    else
+	      false
+	    end
 	end
+
+	def win?
+	    if combo(0,1,2) || combo(3,4,5) || combo(6,7,8) || combo(0,3,6) || combo(1,4,7) || combo(2,5,8) || combo(0,4,8) || combo(2,4,6)
+	        true
+	    else
+	        false
+	    end
+	end
+	
+	def tied?
+	    if !win?
+	        @gameboard.state.each do |pos|
+	            if pos == ' '
+	                return false
+	            end
+	        end
+	        true
+	    end
+	end
+
 
 end
 	
 
 battle = Game.new
 
-battle.play
+winner = false
+
+human = Player.new
+
+comp = Computer.new
+
+input = ' '
+
+battle.gameboard.print
+
+until battle.win? || battle.tied? || input.to_s.upcase == 'Q'
+
+	puts "Where do you want to move?"
+	input = gets.chomp.to_i
+
+	system "clear"
+
+	while !battle.gameboard.available_moves.include?(input - 1)
+		puts "That space is invalid or already taken."
+		input = gets.chomp.to_i
+	end
+
+	human.mark_board(battle.gameboard, input - 1)
+	
+	
+	if battle.win?
+		winner = "human"
+		break
+	elsif battle.tied?
+		break
+	end
+
+	comp.mark_board(battle.gameboard, battle.gameboard.available_moves.sample)
+
+	if battle.win?
+		winner = "computer"
+		break
+	elsif battle.tied?
+		break
+	end
+
+	battle.gameboard.print
+
+end
+
+if winner
+
+	battle.gameboard.print
+
+	puts winner.capitalize + " won!"
+
+else
+
+	puts "Cat's game."
+
+end
+
+
+
