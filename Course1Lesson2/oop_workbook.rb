cd563121b334be8698ceb02e3da8b8fcc5442be5
galaxyhitcher <gregoryheimel@PIPESTONEs.local>
2015-05-20 18:07:05 -0500

    finished oop_workbook

--- Course1Lesson2/oop_workbook.rb
+++ Course1Lesson2/oop_workbook.rb
@@ -180,9 +180,9 @@ class BeesWax
 end
 
 # 5.)
-excited_dog = "excited dog"
-@excited_dog = "excited dog"
-@@excited_dog = "excited dog"
+#excited_dog = "excited dog"
+#@excited_dog = "excited dog"
+#@@excited_dog = "excited dog"
 
 # local variable - excited_dog
   #instance variable - @excited_dog
@@ -322,5 +322,173 @@ tabby.to_s
 
 #4.) 
 
+# class Greeting
+#   def greet(astring)
+#     puts astring
+#   end
+# end
+
+# class Hello < Greeting
+#   def hi
+#     greet("hi")
+#   end
+# end
+
+# class Goodbye < Greeting
+#   def bye
+#     greet("bye")
+#   end
+# end
+
+# 5.)
+
+class KrispyKreme
+  def initialize(filling_type, glazing)
+    @filling_type = filling_type
+    @glazing = glazing
+  end
+
+  def to_s
+    filling_string = @filling_type ? @filling_type : "Plain"
+    glazing_string = @glazing ? " with #{@glazing}" : ''
+    filling_string + glazing_string
+  end
+
+end
+
+donut1 = KrispyKreme.new(nil, nil)
+donut2 = KrispyKreme.new("Vanilla", nil)
+donut3 = KrispyKreme.new(nil, "sugar")
+donut4 = KrispyKreme.new(nil, "chocolate sprinkles")
+donut5 = KrispyKreme.new("Custard", "icing")
+
+puts donut1
+
+puts donut2
+
+puts donut3
+
+puts donut4
+
+puts donut5
+
+# 6.) There is no difference aside from stylistic concerns
+
+# 7.) change the method name self.light_information to self.information
+
+class Light
+  attr_accessor :brightness, :color
 
 
+  def initialize(brightness, color)
+    @brightness = brightness
+    @color = color
+  end
+
+  def self.information
+    "I want to turn on the light with a brightness level of super high and a colour of green"
+  end
+
+end
+
+# Advanced Quiz 1
+
+# 1.) 
+
+class SecretFile
+  attr_reader :data
+
+  def initialize(secret_data, logger)
+    @data = secret_data
+    @logger = logger
+  end
+
+  def data
+    @logger.create_log_entry
+    @data
+  end
+end
+
+# 2.)
+
+module Moveable
+
+
+class WheeledVehicle
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity*@fuel_efficiency
+  end
+end
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class Auto < WheeledVehicle
+  include Moveable
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures along with 
+    super([20,20], 80, 8.0)
+  end
+end
+
+# 3.)
+
+class Seacraft
+  include Moveable
+
+  attr_accessor :hull_count, :propeller_count
+
+  def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    @fuel_efficiency = fuel_efficiency
+    @fuel_capacity = fuel_capacity
+  end
+
+  def range
+    range_by_using_fuel = super
+    return range_by_using_fuel + 10
+  end
+  
+end
+
+class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    # set up 1 hull and 1 propeller
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+class Catamaran < Seacraft
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    super(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+# 4.)  
